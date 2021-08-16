6ab1b527225d597951f7553e66aa389a0be010c7
saezurucrow <40511624+saezurucrow@users.noreply.github.com>
2021-08-16 18:56:16 +0900

    Update scraping.js

--- scraping.js
+++ scraping.js
@@ -7,53 +7,50 @@ class CSV {
 
     if (CSV.isArray(data)) {
       if (0 == data.length) {
-        this.dataType = this.ARRAY
+        this.dataType = this.ARRAY;
       } else if (CSV.isObject(data[0])) {
-        this.dataType = this.OBJECT
+        this.dataType = this.OBJECT;
       } else if (CSV.isArray(data[0])) {
-        this.dataType = this.ARRAY
+        this.dataType = this.ARRAY;
       } else {
-        throw Error('Error: 未対応のデータ型です')
+        throw Error('Error: 未対応のデータ型です');
       }
     } else {
-      throw Error('Error: 未対応のデータ型です')
+      throw Error('Error: 未対応のデータ型です');
     }
 
-    this.keys = keys
+    this.keys = keys;
   }
 
   toString() {
     if (this.dataType === this.ARRAY) {
-      return this.data.map((record) => (
-        record.map((field) => (
-          CSV.prepare(field)
-        )).join(',')
-      )).join('\n')
+      return this.data
+        .map((record) => record.map((field) => CSV.prepare(field)).join(','))
+        .join('\n');
     } else if (this.dataType === this.OBJECT) {
       const keys = this.keys || Array.from(this.extractKeys(this.data));
 
-      const arrayData = this.data.map((record) => (
+      const arrayData = this.data.map((record) =>
         keys.map((key) => record[key])
-      ))
+      );
 
-      return [].concat([keys], arrayData).map((record) => (
-        record.map((field) => (
-          CSV.prepare(field)
-        )).join(',')
-      )).join('\n')
+      return []
+        .concat([keys], arrayData)
+        .map((record) => record.map((field) => CSV.prepare(field)).join(','))
+        .join('\n');
     }
   }
 
   save(filename = 'data.csv') {
     if (!filename.match(/\.csv$/i)) {
-      filename = filename + '.csv'
+      filename = filename + '.csv';
     }
 
     const csvStr = this.toString();
 
-    const bom = new Uint8Array([0xEF, 0xBB, 0xBF]);
+    const bom = new Uint8Array([0xef, 0xbb, 0xbf]);
     const blob = new Blob([bom, csvStr], {
-      'type': 'text/csv'
+      type: 'text/csv'
     });
     const url = window.URL || window.webkitURL;
     const blobURL = url.createObjectURL(blob);
@@ -67,7 +64,9 @@ class CSV {
   }
 
   extractKeys(data) {
-    return new Set([].concat(...this.data.map((record) => Object.keys(record))));
+    return new Set(
+      [].concat(...this.data.map((record) => Object.keys(record)))
+    );
   }
 
   static prepare(field) {
@@ -86,35 +85,41 @@ class CSV {
 try {
   const URL = location.href;
 
-  if (URL != "https://wacca.marv-games.jp/web/music") {
-    throw new Error("表示しているページが違います。プレイデータの楽曲スコアのページを開いて再度試してください。");
+  if (URL != 'https://wacca.marv-games.jp/web/music') {
+    throw new Error(
+      '表示しているページが違います。プレイデータの楽曲スコアのページを開いて再度試してください。'
+    );
   }
 
   let results = [];
   let score_ave = [0, 0, 0, 0];
   let inf_num = 0;
   let tmpScore, tmpResult;
-  let nodes = $('[class*="muuri-item"]');
+  let nodes = $('li.muuri-item');
 
   for (let i = 1; i <= nodes.length; i++) {
-    tmpScore = nodes[i - 1].querySelectorAll('.playdata__score-list__song-info__score');
+    tmpScore = nodes[i - 1].querySelectorAll(
+      '.playdata__score-list__song-info__score'
+    );
 
     tmpResult = {
-      title: nodes[i - 1].querySelector('.playdata__score-list__song-info__name').innerText,
-      n_score: tmpScore[0].innerText.replace(/\s+/g, "").replace("SCORE", ""),
-      h_score: tmpScore[1].innerText.replace(/\s+/g, "").replace("SCORE", ""),
-      e_score: tmpScore[2].innerText.replace(/\s+/g, "").replace("SCORE", ""),
-      i_score: tmpScore[3].innerText.replace(/\s+/g, "").replace("SCORE", "")
-    }
+      title: nodes[i - 1].querySelector(
+        '.playdata__score-list__song-info__name'
+      ).innerText,
+      n_score: tmpScore[0].innerText.replace(/\s+/g, '').replace('SCORE', ''),
+      h_score: tmpScore[1].innerText.replace(/\s+/g, '').replace('SCORE', ''),
+      e_score: tmpScore[2].innerText.replace(/\s+/g, '').replace('SCORE', ''),
+      i_score: tmpScore[3].innerText.replace(/\s+/g, '').replace('SCORE', '')
+    };
 
     score_ave[0] += parseInt(tmpResult.n_score);
     score_ave[1] += parseInt(tmpResult.h_score);
     score_ave[2] += parseInt(tmpResult.e_score);
-    if (tmpResult.i_score != "0") {
+    if (tmpResult.i_score != '0') {
       score_ave[3] += parseInt(tmpResult.i_score);
-      inf_num++
+      inf_num++;
     }
-    results.push(tmpResult)
+    results.push(tmpResult);
   }
 
   score_ave[0] = Math.round(score_ave[0] / nodes.length);
@@ -122,9 +127,11 @@ try {
   score_ave[2] = Math.round(score_ave[2] / nodes.length);
   score_ave[3] = Math.round(score_ave[3] / inf_num);
 
-  alert(`WACCAスコア集計ツール\nAuthor:@chirping_crow\n曲数:${nodes.length}\nINFERNO譜面数:${inf_num}\nAverage(小数点以下四捨五入):\nNORMAL:${score_ave[0]}\nHARD:${score_ave[1]}\nEXPERT:${score_ave[2]}\nINFERNO:${score_ave[3]}\n\nボタンを押すとCSVのダウンロードが始まります。`);
+  alert(
+    `WACCAスコア集計ツール\nAuthor:@chirping_crow\n曲数:${nodes.length}\nINFERNO譜面数:${inf_num}\nAverage(小数点以下四捨五入):\nNORMAL:${score_ave[0]}\nHARD:${score_ave[1]}\nEXPERT:${score_ave[2]}\nINFERNO:${score_ave[3]}\n\nボタンを押すとCSVのダウンロードが始まります。`
+  );
 
-  (new CSV(results)).save('wacca_score.csv');
+  new CSV(results).save('wacca_score.csv');
 } catch (e) {
   alert(e);
 }
